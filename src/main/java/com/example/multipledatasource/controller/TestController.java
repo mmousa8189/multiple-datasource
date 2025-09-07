b04b688dcc40623cab97cf981146ad82713eb212
Mohamed Mousa <mohamedamousa00@gmail.com>
2025-09-07 12:10:11 +0300

    update TestController

--- src/main/java/com/example/multipledatasource/controller/TestController.java
+++ src/main/java/com/example/multipledatasource/controller/TestController.java
@@ -1,7 +1,17 @@
 package com.example.multipledatasource.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.multipledatasource.entity.postgresql.Bug;
+import com.example.multipledatasource.entity.postgresql.Season;
+import com.example.multipledatasource.entity.sqlserver.Flower;
+import com.example.multipledatasource.service.BugService;
+import com.example.multipledatasource.service.FlowerService;
+import com.example.multipledatasource.service.SeasonService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,5 +19,24 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api")
 @AllArgsConstructor
 public class TestController {
+
+    private BugService bugService;
+    private FlowerService flowerService;
+    private SeasonService seasonService;
+
+    @GetMapping("/bugs")
+    public List<Bug> getBugs() {
+        return bugService.getAllBugs();
+    }
+
+    @GetMapping("/flowers")
+    public List<Flower> getFlowers() {
+        return flowerService.getAllFlowers();
+    }
+
+    @GetMapping("/seasons")
+    public List<Season> getSeasons() {
+        return seasonService.getAllSeasons();
+    }
 
 }
